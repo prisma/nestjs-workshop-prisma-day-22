87a26d0cb251ff0f2c80e5b0418eb5963bf7689a
Tasin Ishmam <tasinishmam@gmail.com>
2022-06-14 00:06:21 +0600

    Lesson 2 Task 2 finished

---
+++ src/articles/articles.module.ts
@@ -0,0 +1,13 @@
+// src/articles/articles.module.ts
+
+import { Module } from '@nestjs/common';
+import { ArticlesService } from './articles.service';
+import { ArticlesController } from './articles.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+@Module({
+  controllers: [ArticlesController],
+  providers: [ArticlesService],
+  imports: [PrismaModule],
+})
+export class ArticlesModule {}
