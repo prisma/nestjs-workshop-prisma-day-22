df79f4b06d861f023fa91aa93e08f2bc769cb5eb
Tasin Ishmam <tasinishmam@gmail.com>
2022-06-13 17:40:24 +0600

    Lesson 1 finished

---
+++ src/prisma/prisma.module.ts
@@ -0,0 +1,10 @@
+// src/prisma/prisma.module.ts
+
+import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
