924c823ff6e82bf53f49e8d8a12fa07f3e9e301e
Tasin Ishmam <tasinishmam@gmail.com>
2022-06-14 00:22:08 +0600

    Lesson 2 finished

--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,1 +1,27 @@
-export class Article {}
+// src/articles/entities/article.entity.ts
+
+import { Article } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ArticleEntity implements Article {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  description: string | null;
+
+  @ApiProperty()
+  body: string;
+
+  @ApiProperty()
+  published: boolean;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
