b225b56774fbe03984ddcb9c06334fd65ff27662
Tasin Ishmam <tasinishmam@gmail.com>
2022-06-13 17:40:24 +0600

    Lesson 1 finished

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,13 @@
+// src/prisma/prisma.service.ts
+
+import { INestApplication, Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
