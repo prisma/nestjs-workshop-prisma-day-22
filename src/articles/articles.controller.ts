3e8707257311a620ee7d7b8fa11f02f3d0e1dd09
Tasin Ishmam <tasinishmam@gmail.com>
2022-06-14 15:50:54 +0600

    Lesson 4 finished

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -8,15 +8,18 @@ import {
   Delete,
   NotFoundException,
   ParseIntPipe,
+  UseFilters,
 } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { ArticleEntity } from './entities/article.entity';
+import { PrismaClientExceptionFilter } from 'src/prisma-client-exception.filter';
 
 @Controller('articles')
 @ApiTags('articles')
+@UseFilters(PrismaClientExceptionFilter)
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
 
---
+++ src/prisma-client-exception.filter.ts
@@ -0,0 +1,29 @@
+//src/prisma-client-exception.filter.ts
+import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Prisma } from '@prisma/client';
+import { Response } from 'express';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const message = exception.message.replace(/\n/g, '');
+
+    switch (exception.code) {
+      case 'P2002':
+        const status = HttpStatus.CONFLICT;
+        response.status(status).json({
+          statusCode: status,
+          message: message,
+        });
+        break;
+      // TODO catch other error codes (e.g. 'P2000' or 'P2025')
+      default:
+        // default 500 error code
+        super.catch(exception, host);
+        break;
+    }
+  }
+}
