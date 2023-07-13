bf0c2deb88ef371565cdebe65f180f37acd6fa66
Alex Ruheni <33921841+ruheni@users.noreply.github.com>
2023-07-13 08:30:07 +0000

    chore: remove unused shutdown hooks

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,7 @@
 // src/prisma/prisma.service.ts
 
-import { INestApplication, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
-  }
-}
+export class PrismaService extends PrismaClient { }
